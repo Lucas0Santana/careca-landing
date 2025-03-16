270c3fae10f6072f129c01e3ea5a928885c4f7ff
gabriel.gclc1 <gabriel.gclc@gmail.com>
2025-03-16 18:47:43 -0300

    :bug: FIX: O texto ficava alinhado errado baseado no tamanho que tinha

--- src/Shared/Components/Personalized_card/index.jsx
+++ src/Shared/Components/Personalized_card/index.jsx
@@ -1,37 +1,33 @@
 import { Typography, Card, Flex, Image, Row, Col } from 'antd';
 
 export const Personalized_card = ({ pizza, ingredientes, img_pizza }) => {
-
     const cardStyle = {
         width: 620,
-        height: 210,
+        height: 200,
         backgroundColor: 'blue',
     };
 
-
     return (
-        <Card hoverable size='small' style={cardStyle} styles={{ body: { overflow: 'hidden' } }} variant="borderless">
-            <Flex justify="space-between">
+        <Card hoverable size='small' style={cardStyle} variant="borderless">
+            <Row gutter={20} align={"middle"} justify={"space-around"}>
                 <Row>
-                    <Col span={12}>
-                        <Image alt="avatar" src={img_pizza} width={200} />
-                    </Col>
-
-                    <Col span={12}>
-                        <Row>
-                            <Typography.Title level={3}>
-                                {pizza}
-                            </Typography.Title>
-                        </Row>
-
-                        <Row>
-                            <Typography.Text>
-                                {ingredientes}
-                            </Typography.Text>
-                        </Row>
-                    </Col>
+                    <Image alt="avatar" src={img_pizza} width={200} />
                 </Row>
-            </Flex>
+
+                <Col span={12}>
+                    <Row>
+                        <Typography.Title level={3}>
+                            {pizza}
+                        </Typography.Title>
+                    </Row>
+
+                    <Row>
+                        <Typography.Text>
+                            {ingredientes}
+                        </Typography.Text>
+                    </Row>
+                </Col>
+            </Row>
         </Card>
     )
 }
