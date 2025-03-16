0a19b90abc5f4655b1907dc88a40aa5a656feeba
gabriel.gclc1 <gabriel.gclc@gmail.com>
2025-03-16 18:45:09 -0300

    :sparkles: FEAT: Tradução dos componentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import {  Button, ConfigProvider, Layout } from 'antd';
-
-import { MenuBar, App_Footer } from './Shared/Components';
-import { Environment } from './Shared/Environment';
-import Link from 'antd/es/typography/Link';
-import { Home } from './Pages/Home';
 import { theme } from './Shared/Theme';
+import ptBR from 'antd/locale/pt_BR.js';
+
+import { ConfigProvider, Layout } from 'antd';
+import { MenuBar, App_Footer } from './Shared/Components';
+import { Home } from './Pages/Home';
 
 function App() {
   return (
-    <ConfigProvider theme={theme}>
+    <ConfigProvider theme={theme} locale={ptBR}>
       <Layout style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', backgroundColor: 'black' }}>
         <MenuBar />
           <Home />
