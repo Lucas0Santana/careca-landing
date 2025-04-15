d2d7c3a86b99327471da7d07b006db6463c7b53d
Lucas Santana <lucas1.stoly@gmail.com>
2025-04-15 07:56:36 -0300

    ♻️ refactor: Adicionando AppRoutes ao app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import ptBR from 'antd/locale/pt_BR.js';
 
 import { ConfigProvider, Layout } from 'antd';
 import { MenuBar, App_Footer } from './Shared/Components';
+import { AppRoutes } from './Routes';
 import { Home } from './Pages/Home';
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
     <ConfigProvider theme={theme} locale={ptBR}>
       <Layout style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', backgroundColor: 'black' }}>
         <MenuBar />
-          <Home />
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
         <App_Footer />
       </Layout>
     </ConfigProvider>
