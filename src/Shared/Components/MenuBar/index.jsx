a53fd06379d70965f8dc7b79017e6f56ea294220
Lucas Santana <lucas1.stoly@gmail.com>
2025-05-19 21:33:00 -0300

    ✨ feat: Implementando menu responsivo com Drawer e ajustes de estilo na MenuBar

--- src/Shared/Components/MenuBar/index.jsx
+++ src/Shared/Components/MenuBar/index.jsx
@@ -1,25 +1,66 @@
-  import logo from '../../Images/marcacareca_1.png';
-  import { Header } from "antd/es/layout/layout";
+import { MenuOutlined } from '@ant-design/icons';
+import { Button, Drawer, Menu } from 'antd';
+import { Header } from "antd/es/layout/layout";
+import { useEffect, useState } from 'react';
+import logo from '../../Images/marcacareca_1.png';
 import useApp from "./useApp";
-import { Avatar, Menu } from 'antd';
 
 export const MenuBar = () => {
-    const {
-      headerStyle,
-      menuStyle,
-      menuContainerStyle,
-      logoStyle,
-      itemsLeft,
-      itemsRight,
-    } = useApp();
+  const {
+    headerStyle,
+    menuStyle,
+    menuContainerStyle,
+    logoStyle,
+    itemsLeft,
+    itemsRight,
+  } = useApp();
 
-    return (
-        <Header style={headerStyle}>
+  // const isMobile = window.innerWidth <= 768;
+  const [open, setOpen] = useState(false);
+  // const mdDown = useMediaQuery(theme.breakpoints.down('md'));
+  // const isMobile = useMediaQuery({ maxWidth: 768 });
+  
+  const windowSize = 
+    {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+  const isMobile = 768 > windowSize.width;
+
+    useEffect(() => {
+      console.log(open);
+      }, [open]);
+
+  return (
+    <Header style={headerStyle}>
+      {
+        isMobile ? (
+          <>
+            <Button type="text" icon={<MenuOutlined />} onClick={() => setOpen(!open)} />
+            <Drawer
+              title="Menu"
+              placement="right"
+              onClose={() => setOpen(false)}
+              open={open}
+            >
+              <Menu mode="vertical" items={[...itemsLeft, ...itemsRight]} />
+            </Drawer>
+          </>
+        ) : (
           <div style={menuContainerStyle}>
             <Menu mode="horizontal" items={itemsLeft} style={menuStyle} />
             <img src={logo} alt="Logo" style={logoStyle} />
             <Menu mode="horizontal" items={itemsRight} style={menuStyle} />
-          </div>
-        </Header>
-    )
+          </div >
+        )}
+    </Header>
+  )
 }
+// <Header style={headerStyle}>
+//   <div style={menuContainerStyle}>
+//     <Menu mode="horizontal" items={itemsLeft} style={menuStyle} />
+//     <img src={logo} alt="Logo" style={logoStyle} />
+//     <Menu mode="horizontal" items={itemsRight} style={menuStyle} />
+//   </div>
+// </Header>
