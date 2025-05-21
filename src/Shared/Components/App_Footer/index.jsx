9c3349a9038de0ccf05ca3602f79d568aad9f7f2
Lucas Santana <lucas1.stoly@gmail.com>
2025-05-20 21:47:44 -0300

    ✨ feat: Adicionando estilos CSS ao componente App_Footer e removendo estilos inline

--- src/Shared/Components/App_Footer/index.jsx
+++ src/Shared/Components/App_Footer/index.jsx
@@ -5,65 +5,47 @@ import Link from "antd/es/typography/Link"
 import { Environment } from "../../Environment"
 import logo from "../../Images/marcacareca_1.png"
 
+import styles from "./App_Footer.module.css"
+
 export const App_Footer = () => {
 
-    const style = {
-        backgroundColor: Environment.BASE_COLOR,
-        marginBottom: '0px',
-    }
-
-    const link = {
-        color: 'white',
-        fontSize: '16px',
-    }
-
-    const colStyle = {
-        fontSize: '18px',
-        width: '100%',
-        marginRight: '50%',
-    }
-
-    const imgStyle = {
-        width: '200px',
-    }
 
     return (
-        <Footer style={style}>
-            <Row  gutter={100} align={'middle'} style={{ fontSize: '18px' }}>
+        <Footer className={styles.style} style={{ backgroundColor: Environment.BASE_COLOR }}>
+            <Row  gutter={100} align={'middle'} className={{ fontSize: '18px',  }}>
                 <Col>
-                    <img src={logo} alt="" style={imgStyle} />
-                    {/* <Avatar size={200} style={{ padding: '25px', }} src={logo} alt="Logo"></Avatar> */}
+                    <img src={logo} alt="" className={styles.imgStyle} />
                 </Col>
 
                 <Col>
-                    <Typography style={colStyle}>
+                    <Typography className={styles.colStyle}>
                         Acesso Rápido
                     </Typography>
 
                     <Row>
-                        <Link style={link}> Início </Link>
+                        <Link className={styles.link}> Início </Link>
                     </Row>
                     <Row>
-                        <Link style={link}> Cardápio </Link>
+                        <Link className={styles.link}> Cardápio </Link>
                     </Row>
                     <Row>
-                        <Link style={link} href="https://api.whatsapp.com/send?phone=5579988520069" target="_blank"> Fazer Pedido </Link>
+                        <Link className={styles.link} href="https://api.whatsapp.com/send?phone=5579988520069" target="_blank"> Fazer Pedido </Link>
                     </Row>
                 </Col>
 
                 <Col>
-                    <Typography style={colStyle}>
+                    <Typography className={styles.colStyle}>
                         Fale Conosco
                     </Typography>
                     
                     <Row>
-                        <Link style={link} href="https://api.whatsapp.com/send?phone=5579988520069" target="_blank"> <WhatsAppOutlined /> (79) 9 8852-0069 </Link>
+                        <Link className={styles.link} href="https://api.whatsapp.com/send?phone=5579988520069" target="_blank"> <WhatsAppOutlined /> (79) 9 8852-0069 </Link>
                     </Row>
                     <Row>
-                        <Link style={link}> <PhoneOutlined /> (79) 9 9641-6992 </Link>
+                        <Link className={styles.link}> <PhoneOutlined /> (79) 9 9641-6992 </Link>
                     </Row>
                     <Row>
-                        <Link style={link} href="https://www.instagram.com/pizzariadocareca1/" target="_blank"> <InstagramOutlined /> @pizzariadocareca1 </Link>
+                        <Link className={styles.link} href="https://www.instagram.com/pizzariadocareca1/" target="_blank"> <InstagramOutlined /> @pizzariadocareca1 </Link>
                     </Row>
                 </Col>
 
